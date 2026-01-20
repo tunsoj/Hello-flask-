9fbefd39f70143df0ddc29fd8dac977e531336f3
90tunios <90tunios@gafe.molndal.se>
2026-01-20 10:54:34 +0000

    Initial commit: Hello Flask application

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,54 +1,50 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
+  # To learn more about how to use Nix to configure your environment
+  # see: https://developers.google.com/idx/guides/customize-idx-env
+  
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+
+  # Use https://search.nixos.org/packages to find packages.
+  # We are adding python3 and pip for our Flask application.
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.python3
+    pkgs.pip
   ];
+
   # Sets environment variables in the workspace
   env = {};
+
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id".
+    # We are adding the official Python extension.
     extensions = [
-      # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
+      "ms-python.python"
     ];
-    # Enable previews
+
+    # Enable previews for our web application.
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          # This command runs the Flask development server.
+          # It uses the $PORT environment variable provided by IDX to make the app available for preview.
+          command = ["flask" "run" "--host" "0.0.0.0" "--port" "$PORT"];
+          manager = "web";
+        };
       };
     };
+
     # Workspace lifecycle hooks
     workspace = {
-      # Runs when a workspace is first created
+      # Runs when a workspace is first created.
+      # This command installs the Python packages listed in requirements.txt.
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
+        pip-install = "pip install -r requirements.txt";
       };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
+
+      # Runs when the workspace is (re)started.
+      onStart = {};
     };
   };
 }
